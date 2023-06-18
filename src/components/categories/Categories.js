eb209651e26c9be234f1814aae018ac0989116d7
“aisse” <aisse.camara@gmail.com>
2023-06-18 18:49:37 +0200

    Création du component Categories

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,60 @@
+const Categories = [
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "React"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "Symfony"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "Javascript"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "React"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "React"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "React"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "React"
+    },
+    {
+        id:1,
+        title: "Nom du projet",
+        image : "./",
+        lien : "./",
+        Category : "CMS"
+    },
+]
+ 
+export default Categories ;

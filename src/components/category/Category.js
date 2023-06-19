2d4510a5b5a3168ea8031eca609ea600dea1fb79
“aisse” <aisse.camara@gmail.com>
2023-06-19 19:41:18 +0200

    Mise en page des blocs category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,45 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Categories from "../categories/Categories";
 
 const Category = () => {
+    const [data,SetData]=useState(Categories);
+
     return ( 
         
+        <div className="flex flex-wrap justify-center gap-4 mt-12 ">
+            
+        {data.map((values)=>{
+            const {id,title,image,lien}=values;
+            return(
+                <div className=" w-[436px] h-[327px] bg-primary rounded-3xl" key={id}>
+                    <h1>{title}</h1>
+                    <img src={image} alt="..." className="w-96 h-48 rounded-3xl" />
+                    <a href={lien}>
+                        <button >voir le projet</button>
+                    </a>
+                </div>
+                )
+            })}
+                </div>
+           
+                
 
-        <div className="flex flex-row gap-12 items-center w-11/12 h-20 mt-6 ml-auto mr-auto bg-primary rounded-3xl snap-x ">
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="Reactjs" value={"Reactjs"} />
-                <label htmlFor="Reactjs" className="text-tertiary text-xl font-bold ml-2">Reactjs</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="Symfony" value={"Symfony"} />
-                <label htmlFor="Symfony" className="text-tertiary text-xl font-bold ml-2">Symfony</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="Angular" value={"Angular"} />
-                <label htmlFor="Angular" className="text-tertiary text-xl font-bold ml-2">Angular</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="Javascript" value={"Javascript"} />
-                <label htmlFor="Javascript" className="text-tertiary text-xl font-bold ml-2">Javascript</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="html/css" value={"html/css"} />
-                <label htmlFor="html/css" className="text-tertiary text-xl font-bold ml-2">Html/Css</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="Javascript" value={"Javascript"} />
-                <label htmlFor="Javascript" className="text-tertiary text-xl font-bold ml-2">Javascript</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="CMS" value={"CMS"} />
-                <label htmlFor="CMS" className="text-tertiary text-xl font-bold ml-2">CMS</label>
-            </div>
-            <div className="scroll-ml-6 snap-start">
-                <input type="radio" id="WebDesign" value={"WebDesign"} />
-                <label htmlFor="WebDesign" className="text-tertiary text-xl font-bold ml-2">WebDesign</label>
-            </div>
-        </div>
-        
      );
 }
  

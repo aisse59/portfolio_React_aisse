b0309901488933845b489f6f8fa8ac48525fd099
“aisse” <aisse.camara@gmail.com>
2023-06-29 15:46:07 +0200

    Mise en forme des component Input et Textarea

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({type,placeholder, name}) => {
     return ( 
-        <h1>Input</h1>
+        <input 
+        className="w-full h-16 outline-0 py-3 px-4 mt-2 mb-2 rounded-3xl text-primary bg-quaternary placeholder-primary font-semibold text-xs"
+        type={type} 
+        placeholder={placeholder}
+        name={name}
+        required
+        />
      );
 }
  
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,48 @@
 import React from "react";
+import Input from "../components/input/Input";
+import Textarea from "../components/textarea/Textarea";
+
 
 const Contact = () => {
     return ( 
-        <h1>Contact</h1>
+        <>
+            <form action="">
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="nom" className=" font-semibold text-sm text-quaternary ">Votre nom</label>
+                        <Input 
+                        type={'text'} 
+                        placeholder={'Quel est votre nom'}
+                        name={'nom'}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="prenom" className="font-semibold text-sm text-quaternary">Votre prénom</label>
+                        <Input 
+                        type={'text'} 
+                        placeholder={'Quel est votre prénom'}
+                        name={'prenom'}
+                        />
+                    </div>
+                </div>
+
+                    <label htmlFor="email" className="font-semibold text-sm text-quaternary">Votre Email</label>
+                    <Input 
+                    type={'email'} 
+                    placeholder={'Quel est votre email'}
+                    name={'email'}
+                    />
+
+                    <label htmlFor="message" className="font-semibold text-sm text-quaternary">Votre Message</label>
+                    <Textarea
+                    name={'message'}
+                    placeholder={'Laissez-moi un message pour discuter de vos besoins en recrutement et pour explorer comment mes compétences pourraient contribuer à votre équipe. Votre message est le premier pas vers une collaboration fructueuse.'}
+                    />
+                    <div className="">
+                        <button className="w-48 h-16 outline-0 py-3 px-4  rounded-3xl text-primary bg-quaternary  font-semibold text-xs" type="submit">Envoyer</button>
+                    </div>
+            </form>
+        </>
      );
 }
  

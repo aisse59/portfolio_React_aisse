885c0f251a31ddf7db509294a8fa9184ec259854
“aisse” <aisse.camara@gmail.com>
2023-07-20 11:08:22 +0200

    responsive du component Category OK

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -22,48 +22,48 @@ const Category = () => {
         <ul className="flex flex-row gap-12 items-center w-11/12 h-20 mt-6 ml-auto mr-auto  bg-primary rounded-3xl overflow-y-auto hide-scrollbar ">
             <li className="flex flex-row pl-3">
                 <input type="radio" id="Reactjs" name="lolo" value={"Reactjs"} onClick={()=>filterResult('React')} />
-                <label htmlFor="Reactjs" className="text-quaternary text-xl font-bold ml-2">Reactjs</label>
+                <label htmlFor="Reactjs" className="text-quaternary sm:text-base md:text-lg lg:text-xl font-bold ml-2">Reactjs</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="Symfony" name="lolo" value={"Symfony"} onClick={()=>filterResult('Symfony')} />
-                <label htmlFor="Symfony" className="text-quaternary text-xl font-bold ml-2">Symfony</label>
+                <label htmlFor="Symfony" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">Symfony</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="Angular" name="lolo" value={"Angular"} onClick={()=>filterResult('Angular')} />
-                <label htmlFor="Angular" className="text-quaternary text-xl font-bold ml-2">Angular</label>
+                <label htmlFor="Angular" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">Angular</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="Javascript" name="lolo" value={"Javascript"} onClick={()=>filterResult('Javascript')} />
-                <label htmlFor="Javascript" className="text-quaternary text-xl font-bold ml-2">Javascript</label>
+                <label htmlFor="Javascript" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">Javascript</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="html/css" name="lolo" value={"html/css"} onClick={()=>filterResult('Html/Css')} />
-                <label htmlFor="html/css" className="text-quaternary text-xl font-bold ml-2">Html/Css</label>
+                <label htmlFor="html/css" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">Html/Css</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="Certification" name="lolo" value={"Certification"} onClick={()=>filterResult('Certification')}/>
-                <label htmlFor="Certification" className="text-quaternary text-xl font-bold ml-2">Certification</label>
+                <label htmlFor="Certification" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">Certification</label>
             </li>
             <li className="flex flex-row">
                 <input type="radio" id="CMS" name="lolo" value={"CMS"} onClick={()=>filterResult('CMS')}/>
-                <label htmlFor="CMS" className="text-quaternary text-xl font-bold ml-2">CMS</label>
+                <label htmlFor="CMS" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">CMS</label>
             </li>
             <li className="flex flex-row pr-3">
                 <input type="radio" id="WebDesign" name="lolo" value={"WebDesign"} onClick={()=>filterResult('WebDesign')} />
-                <label htmlFor="WebDesign" className="text-quaternary text-xl font-bold ml-2">WebDesign</label>
+                <label htmlFor="WebDesign" className="text-quaternary sm:text-base md:text-lg lg:text-xl  font-bold ml-2">WebDesign</label>
             </li>
             
         </ul>
         
-        <div className="flex flex-wrap w-[80%] h-screen mx-auto justify-start gap-4  mt-12  ">
+        <div className="flex flex-wrap justify-around w-full pb-5  mx-auto gap-4 pt-12  ">
         
         {data.map((values)=>{
             const {id,title,image,lien}=values;
             return(
-                <div className=" w-[45%] h-[327px] bg-primary shadow-lg shadow-primary rounded-3xl py-8  " key={id}>
+                <div className=" sm:w-[60%] lg:w-[45%] h-[327px] bg-primary shadow-lg shadow-primary rounded-3xl py-8  " key={id}>
                     <h1 className="text-quaternary text-sm font-semibold pl-8">{title}</h1>
                     <div className="grid justify-items-center gap-4 pt-4">
-                        <img src={image} alt="..." className="object-cover w-[379px] h-[197px] rounded-3xl " />
+                        <img src={image} alt="..." className="object-cover w-[90%] h-[197px] rounded-3xl " />
                         <a href={lien} >
                             <button className="bg-quaternary text-secondary w-36 h-8 rounded-3xl text-xs font-semibold hover:bg-secondary hover:text-quaternary duration-700">voir le projet</button>
                         </a>

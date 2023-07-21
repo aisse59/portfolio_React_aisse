603aae0910997e97be3205c8949f0a8d2655e52f
“aisse” <aisse.camara@gmail.com>
2023-07-21 11:24:41 +0200

    Responsive component Home OK

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,20 +15,20 @@ const Home = () => {
 
                     {/* block nom,prénom et fonction */}
                     <div className="ml-10">
-                        <h1 className="text-quaternary text-4xl font-normal" >
+                        <h1 className="text-quaternary text-2xl md:text-4xl font-normal" >
                             Aïssé Camara
                         </h1>
-                        <h2 className="text-quaternary text-5xl font-semibold italic" >
+                        <h2 className="text-quaternary text-3xl md:text-5xl font-semibold italic" >
                             Développeuse web
                         </h2>
                     </div>
                     {/* text de présentation */}
                     <div className="w-3/5  ml-10">
-                        <p className="text-quaternary text-xl" >
+                        <p className="text-quaternary text-base md:text-xl" >
                             Bienvenue dans mon portfolio en tant que développeuse web passionnée ! Je suis dévouée à créer des expériences en ligne uniques et fonctionnelles. Parcourez mes projets variés qui allient créativité, code et innovation. Je suis ravie de partager avec vous mon parcours et mes compétences techniques. Préparez-vous à plonger dans le monde dynamique du développement web !  
                         </p>
                     </div>
-                    <button onClick={handleDowload} className="w-64 h-20 bg-quaternary rounded-3xl shadow-md text-secondary font-semibold text-xl  ml-10" >
+                    <button onClick={handleDowload} className="w-64 h-20 bg-quaternary rounded-3xl shadow-md text-secondary font-semibold text-xl  ml-10 hover:bg-secondary hover:text-quaternary duration-700" >
                         Télécharger mon CV
                     </button>
                </div>

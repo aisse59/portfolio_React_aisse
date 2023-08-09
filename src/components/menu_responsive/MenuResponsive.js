c565381e97201ba15a72943e07b2bfdce5d031da
“aisse” <aisse.camara@gmail.com>
2023-08-09 13:25:15 +0200

    Menu Ok reste manque active lien

--- src/components/menu_responsive/MenuResponsive.js
+++ src/components/menu_responsive/MenuResponsive.js
@@ -11,6 +11,7 @@ import contact from '../../icons/Contact.svg';
 import twitter from '../../icons/Twitter.svg';
 import github from '../../icons/GitHub.svg';
 import linkedin from '../../icons/LinkedIn.svg';
+import copyright from '../../icons/Copyright.svg';
 
 
 const MenuResponsive = () => {
@@ -64,12 +65,14 @@ const MenuResponsive = () => {
                     {clicked ? <img src={close} alt="close" /> : <img src={menu} alt="menu" />}
                 </div>
             </div>
+
                   {/* div image et nom,prenom */}
             <div className="grid justify-items-center gap-8 ">
                 <img src={aisse} alt="aisse" className="w-40 h-40 border-4 rounded-full  border-quaternary object-cover mt-4" />
                 <h1 className=" text-center text-quaternary font-semibold text-4xl mb-12" >Aïssé Camara</h1>
             </div>
-                <ul>
+
+                <ul className="grid justify-center gap-2">
                     <li > 
                         <Link 
                             to="/accueil" 
@@ -78,9 +81,8 @@ const MenuResponsive = () => {
                             : 'flex flex-row gap-8 text-2xl font-semibold hover:text-tertiary duration-700 '}
                             onClick={() => handleClick('accueil')} 
                             >
-                            <img src={accueil} alt="accueil svg" /> 
-                            <span 
-                            >Accueil</span>
+                            <img src={accueil} alt="accueil svg"  /> 
+                            <span>Accueil</span>
                         </Link>
                     </li>
                     <li > 
@@ -118,24 +120,30 @@ const MenuResponsive = () => {
                         </Link>
                     </li>
                 </ul>
-                {/* RESEAUX SOCIAUX */}
-            <div className="flex flex-row justify-around mt-20 mb-5">
-                <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
-                    <a href={githubProfilLink} target="_blank" rel="noopener noreferrer">
-                        <img src={github} alt="github svg"  />
-                    </a>
+                    
+                    {/* RESEAUX SOCIAUX */}
+                <div className="flex flex-row gap-5 justify-around mt-12 mb-5">
+                    <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
+                        <a href={githubProfilLink} target="_blank" rel="noopener noreferrer">
+                            <img src={github} alt="github svg"  />
+                        </a>
+                    </div>
+                    <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
+                        <a href={twitterProfilLink} target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter svg" />
+                        </a>
+                    </div>
+                    <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
+                        <a href={linkedinProfilLink} target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin svg" />
+                        </a>
+                    </div>
                 </div>
-                <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
-                    <a href={twitterProfilLink} target="_blank" rel="noopener noreferrer">
-                        <img src={twitter} alt="twitter svg" />
-                    </a>
+                    {/* copyryght */}
+                <div id="copyright" className="flex flex-row gap-2 justify-center absolute  bottom-3 ">
+                    <img src={copyright} alt="copyright svg" />
+                    <span className="text-quaternary text-xs">copyright,Aïssé Camara- 2023</span>
                 </div>
-                <div className="w-14 h-14 bg-quaternary rounded-full grid place-items-center ">
-                    <a href={linkedinProfilLink} target="_blank" rel="noopener noreferrer">
-                        <img src={linkedin} alt="linkedin svg" />
-                    </a>
-                </div>
-            </div>
             </div>
 
         </nav>
